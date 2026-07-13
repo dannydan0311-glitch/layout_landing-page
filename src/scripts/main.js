22e28e1687933f92c7fd08570cb5688a69ae4e3a
Yana <batia15061983@gmail.com>
2026-07-13 15:04:07 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const elements = document.querySelectorAll(
+  ".pre-animation--right, .pre-animation--left, .pre-animation--appear"
+);
+
+const observer = new IntersectionObserver((blocks, obsrver) => {
+  blocks.forEach((block) => {
+    if (block.isIntersecting) {
+
+      if (block.target.classList.contains("pre-animation--right")) {
+        block.target.classList.add("animation--right");
+      }
+
+      if (block.target.classList.contains("pre-animation--left")) {
+        block.target.classList.add("animation--left");
+      }
+
+      if (block.target.classList.contains("pre-animation--appear")) {
+        block.target.classList.add("animation--appear");
+      }
+
+      observer.unobserve(block.target);
+    }
+  });
+});
+
+elements.forEach((element) => {
+  observer.observe(element);
+});
