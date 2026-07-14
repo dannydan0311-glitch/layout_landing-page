af13b18f84b61daf105f108d79a3bf333d48f08d
Yana <batia15061983@gmail.com>
2026-07-14 15:38:06 +0200

    fixed all adaptive issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
+/* global IntersectionObserver */
 'use strict';
 
 const elements = document.querySelectorAll(
-  ".pre-animation--right, .pre-animation--left, .pre-animation--appear"
+  '.pre-animation--right, .pre-animation--left, .pre-animation--appear',
 );
 
 const observer = new IntersectionObserver((blocks, obsrver) => {
   blocks.forEach((block) => {
     if (block.isIntersecting) {
-
-      if (block.target.classList.contains("pre-animation--right")) {
-        block.target.classList.add("animation--right");
+      if (block.target.classList.contains('pre-animation--right')) {
+        block.target.classList.add('animation--right');
       }
 
-      if (block.target.classList.contains("pre-animation--left")) {
-        block.target.classList.add("animation--left");
+      if (block.target.classList.contains('pre-animation--left')) {
+        block.target.classList.add('animation--left');
       }
 
-      if (block.target.classList.contains("pre-animation--appear")) {
-        block.target.classList.add("animation--appear");
+      if (block.target.classList.contains('pre-animation--appear')) {
+        block.target.classList.add('animation--appear');
       }
 
       observer.unobserve(block.target);
